1847fdb2a10bc8d13d9e9898461dcc81c96026ac
pejda93 <bladeofweed93@yahoo.com>
2021-08-15 16:41:13 +0200

    kacenje domaceg

---
+++ IT Domaci/Nedelja3/src/Zadatak.java
@@ -0,0 +1,5 @@
+public class Zadatak {
+    public static void main(String[] args) {
+        System.out.println("mladen");
+    }
+}
--- IT Domaci/src/Geometrija.java
+++
@@ -1,5 +0,0 @@
-public class Geometrija {
-    public static void main(String[] args) {
-        System.out.println("Geometrija");
-    }
-}
