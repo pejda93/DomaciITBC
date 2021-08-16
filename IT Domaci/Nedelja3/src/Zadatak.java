3d2ae344d750ad46cd9182bbefcf53dd4396fa68
pejda93 <bladeofweed93@yahoo.com>
2021-08-16 17:12:01 +0200

    resen 12. zadatak

--- IT Domaci/Nedelja3/src/Zadatak.java
+++ IT Domaci/Nedelja3/src/Zadatak.java
@@ -1,5 +1,24 @@
 public class Zadatak {
+
+
+    public static boolean Palindrom(String broj){
+        char[] niz = broj.toCharArray();
+
+        for (int i = 0; i < niz.length; i++) {
+            if(niz[i]!=niz[niz.length-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("mladen pejic");
+        String broj = "1134311";
+
+        System.out.println(Palindrom(broj));
+
+
+
     }
 }
